fca5436fb30b565a894d4dfc2cdf72086d62eb78
realsam007 <samarthbhardwaj2@gmail.com>
2024-10-13 00:41:50 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,62 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyCxlTO2j_UtEhpTDu90yQBo24tMx_o43Wg",
+  authDomain: "myprojectweek7.firebaseapp.com",
+  projectId: "myprojectweek7",
+  storageBucket: "myprojectweek7.appspot.com",
+  messagingSenderId: "720162503985",
+  appId: "1:720162503985:web:6ea2a754843391957b4410",
+  measurementId: "G-7508LHGF6W"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth, additionalInformation = {}) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      })
+    }
+    catch (error) {
+      console.log('error in creating ', error.message)
+    }
+  }
+
+  return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signinAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password)
+}
